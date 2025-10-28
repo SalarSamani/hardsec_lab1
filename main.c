94be42f4ad5ffb6a0389bb1101514558594ac797
Salar Samani <soltanisamani@gmail.com>
2025-10-28 02:48:26 +0100

    Implement Task1

--- main.c
+++ main.c
@@ -10,11 +10,18 @@
 #include <x86intrin.h>
 #include "asm.h"
 
+#include <time.h>     // for srand()/rand()
+#include <limits.h>   // for SIZE_MAX
+#include <stdint.h>   // for uint64_t
+
 #define BUFFER_SIZE (1ULL<<30)
 #define MMAP_FLAGS (MAP_ANONYMOUS|MAP_PRIVATE|MAP_POPULATE|MAP_HUGETLB|(30<<MAP_HUGE_SHIFT))
 #define HUGETLBFS "/mnt/lab1/buff"
 #define TASK_NOT_IMPLEMENTED -9
 
+#define N_MEASUREMENTS (1 << 18)            // how many probe addresses / samples to collect
+#define ROUNDS_PER_PAIR 100                 // how many timing trials per address pair
+#define ADDR_ALIGN 64                       // align probes to 64B so we hit cacheline boundaries
 
 
 //-----------------------------------------------------------------
@@ -23,11 +30,44 @@
 *   (It makes it easier for us when grading and debugging) 
 */
 size_t time_accesses(char* addr1, char* addr2, size_t rounds) {
-    size_t delta_t = 0;
-/*
- *	__insert code here__
- * */
-    return delta_t;
+    size_t best = SIZE_MAX;  // track the minimum timing we see
+
+    for (size_t r = 0; r < rounds; r++) {
+
+        // Evict both addresses from all cache levels
+        clflush(addr1);
+        clflush(addr2);
+
+        // Make sure flushes are done before we start the timer
+        mfence();
+        lfence();
+
+        // Timestamp before the memory accesses
+        uint64_t t0 = rdtsc();
+        lfence(); // serialize rdtsc with following loads
+
+        // Force actual loads from DRAM
+        // volatile prevents compiler from optimizing these loads away
+        volatile unsigned char v1 = *(volatile unsigned char*)addr1;
+        volatile unsigned char v2 = *(volatile unsigned char*)addr2;
+
+        (void)v1;
+        (void)v2;
+
+        lfence(); // make sure loads have finished
+
+        // Timestamp after
+        uint64_t t1 = rdtsc();
+        lfence();
+
+        size_t delta = (size_t)(t1 - t0);
+
+        if (delta < best) {
+            best = delta;
+        }
+    }
+
+    return best;
 }
 
 //-----------------------------------------------------------------
@@ -38,14 +78,44 @@ size_t time_accesses(char* addr1, char* addr2, size_t rounds) {
  * */
 void export_times(char* buff) {
 
-/*
- *	__insert code here__
- * */
+    // Prefault / warm the 1GB hugepage so we don't measure page faults later.
+    //    We just write one byte per 4KB page.
+    for (size_t off = 0; off < BUFFER_SIZE; off += 4096) {
+        buff[off] = (char)(off & 0xff);
+    }
 
- //  export the output in the following format: 
- //  fprintf(stdout, "%p,%p,%ld\n",base_addr, probe_addr, time);
+    // Seed RNG (not crypto, just spreading probes around the 1GB region)
+    srand((unsigned int)time(NULL));
 
+    // Choose one fixed base address.
+    //    You can pick buff itself, or some offset; doesn't really matter.
+    char* base_addr = buff;
+
+    // Collect a bunch of measurements.
+    for (size_t i = 0; i < N_MEASUREMENTS; i++) {
+
+        // Generate a "random" offset in [0, BUFFER_SIZE)
+        // We can't just do rand() % BUFFER_SIZE because rand() is only 31 bits.
+        // Mix two rand() calls to get more entropy.
+        size_t r_lo = (size_t)rand();
+        size_t r_hi = (size_t)rand();
+        size_t rnd  = (r_hi << 31) ^ r_lo;  // simple mash-up
+        size_t off  = rnd % (BUFFER_SIZE - ADDR_ALIGN);
+
+        // Align to 64B boundary to reduce noise from sub-line effects
+        off &= ~(ADDR_ALIGN - 1);
+
+        char* probe_addr = buff + off;
+
+        // Measure timing for base_addr <-> probe_addr
+        size_t t = time_accesses(base_addr, probe_addr, ROUNDS_PER_PAIR);
+
+        // Export in format expected by plotter.py
+        //    NOTE: %p prints the pointer value, %zu prints size_t.
+        fprintf(stdout, "%p,%p,%zu\n", base_addr, probe_addr, t);
+    }
 }
+
 
 
 //-----------------------------------------------------------------
